bc3900a7b62436bde76a0ea23b6a3d076ab2dfde
xu_zan <Zan.Xu@hella.com>
2013-03-02 23:24:57 +0800

    Add the main() portal program file and configuration.h file for this Visual Studio C subproject.

---
+++ FW_Simulation_Testing/FW_Simulation_Testing_Main.c
@@ -0,0 +1,41 @@
+/***********************************************************************************************************************
+ * Project		: Renesas_MCU_uPD70F3376_Switch_Matrix_Controller
+ *				: Firmware simulation testing project.
+ * 
+ * File name	: FW_Simulation_Testing_Main.c
+ * Description	: Simulate to test the firmware of Renesas_MCU_uPD70F3376_Switch_Matrix_Controller.
+ *				: This file is the portal of firmware simulation testing project.
+ * Creator		: XU ZAN [HSE-OT]
+ * Creation date: Sat.	March 2, 2013
+ * Copyright(C)		2010 --- 2013	Hella Shanghai Electronics Co., Ltd.
+ * All rights reserved.
+ *
+ ***********************************************************************************************************************/
+
+#include <stdio.h>
+
+#include "..\protocol\Parse_UART2_Message.h"
+
+
+/*******************************************************
+ * The portal of program : main() function
+ *******************************************************/
+int main(int argc, char *argv[])
+{
+	char *sSimulationTestingCmdMesg    = "$ACTIon:SWITch 0x01:12 ON!";
+									// = "$ACTIon:ADC CH2:VOLT?!";
+									// = "$ACTIon:ADC *:VOLT?!";
+									// = "$ACTIon:ADC CH1:VOLT?;ADC CH3:VOLT?;ADC CH7:VOLT?!";
+									// = "$ACTIon:ADC CH5:CURRent 82.5?!";
+									// = "$ACTIon:ADC CH7:CURRent 128.35?;ADC CH8:CURRent 65.80?!";
+	int iResult = 0;
+
+	iResult = Parse_UART2_Received_Message(sSimulationTestingCmdMesg);
+
+	printf("Current result : %d\n", iResult);
+
+
+/*******************************/
+	return 0;
+}
+
---
+++ FW_Simulation_Testing/configuration.h
@@ -0,0 +1,44 @@
+/***************************************************************************************
+ * Project		: FW_Simulation_Testing
+ * 
+ * File name	: configuration.h
+ * Description	: Configure some macros and type-definitions in this file, to make the 
+ *				: simulation-debugging available & possible in Visual Studio environment
+ *				: Because these program files are built in Renesas CubeSuite+ MCU 
+ *				: firmware compiling environment.
+ * Creator		: XU ZAN [HSE-OT]
+ * Creation date: Sat.	March 2, 2013
+ * Copyright(C)		2010 --- 2013	Hella Shanghai Electronics CO., Ltd.
+ * All rights reserved.
+ *
+ ***************************************************************************************/
+
+#ifndef _CONFIGURATION_H
+#define _CONFIGURATION_H
+
+	typedef unsigned int	UINT;
+	typedef UINT			DWORD;
+	typedef signed int		SINT;
+
+	typedef unsigned short	USHORT;
+	typedef signed short	SHORT;
+	typedef USHORT			WORD;
+
+	typedef unsigned char	UCHAR;
+	typedef UCHAR			BYTE;
+	typedef signed char		SCHAR;
+
+	typedef unsigned char	BOOL;
+	typedef unsigned short	MD_STATUS;
+
+	#define MD_ON	1
+	#define MD_OFF	0
+
+	enum LEVEL
+	{
+		LOW		= 0,
+		HIGH	= 1
+	};
+
+#endif	/*  _CONFIGURATION_H  */
+
