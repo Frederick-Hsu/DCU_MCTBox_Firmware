0852f2236fdaeacc4339fb798ee1092b7bf165ab
xu_zan <Zan.Xu@hella.com>
2013-03-06 19:35:40 +0800

    Catalog and divide the DIN command into 2 groups : 'Single-Channel state querying'  and  'Multi-Channel state querying'.
    then we can use 2 general functions  handling_SingleCH_DIN_cmd()  and  handling_SingleCH_DIN_cmd()  to implement them,
    
    and what's more, function handling_SingleCH_DIN_cmd() can invoke handling_SingleCH_DIN_cmd() to recyclablily execute 1 command unit.
    
    This method can improve the reusability,  improve the efficiency of function calling.

--- protocol/handling_DIO_cmd.c
+++ protocol/handling_DIO_cmd.c
@@ -14,7 +14,25 @@
 
 #include "handling_DIO_cmd.h"
 
+int handling_SingleCH_DIN_cmd(char	*sARGIN_DinSingleChCmdMesg, 
+							  char	*sARGOUT_DinSingleChStateResponseMesg)
+{
+	int iError = 0;
 
+
+/*******************************/
+	return iError;
+}
+
+int handling_MultiCH_DIN_cmd(char *ARGIN_DinMultiChCmdMesg,
+							 char *ARGOUT_DinMultiChStateResponseMesg)
+{
+	int iError = 0;
+
+
+/*******************************/
+	return iError;
+}
 /*
  * END OF FILE <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
  */
