454be66ff50a237515031c455ce8aa9a8d21f1b6
git_server_administrator_xuzan <Zan.Xu@hella.com>
2013-03-08 16:31:44 +0800

    Fine-tune some property for the Code::Block project.

--- protocol/handling_DIO_cmd.c
+++ protocol/handling_DIO_cmd.c
@@ -1,11 +1,11 @@
 /**********************************************************************************
- * Module		: DIO command-handling module
+ * Module	: DIO command-handling module
  *
  * File name	: handling_DIO_cmd.c
- * Description	: In this module, its purpose is to dedicatedly parse and handle 
+ * Description	: In this module, its purpose is to dedicatedly parse and handle
  *				: Digital-IN & Digital-OUT catalog of commands.
  *				: this file implements these functions.
- * Creator		: XU ZAN [HSE-OT]
+ * Creator	: XU ZAN [HSE-OT]
  * Creation date: Wed.	March 6, 2013
  * Copyright(C)		2010 --- 2013	Hella Shanghai Electronics Co., Ltd.
  * All rights reserved.
