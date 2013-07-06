bcce7d57598a50223b2392d64850fe192fb9ff58
XU ZAN <zan.xu@hella.com>
2013-07-06 17:11:32 +0800

    Add the "parsing/handling PWM command" files : handling_PWM_cmd.c (.h) into protocol module.

---
+++ protocol/handling_PWM_cmd.c
@@ -0,0 +1,16 @@
+/******************************************************************************************
+ * File name	: handling_PWM_cmd.c
+ * Description	: Parsing the com port commands about PWM, and handling the PWM commands.
+ * Creator	: XU ZAN
+ * Creation date: Tue.	June 25, 2013
+ * Copyright(C)	2010 --- 2013	Hella Shanghai Electronics Co., Ltd.
+ * All rights reserved.
+ *
+ ******************************************************************************************/
+
+#include "handling_PWM_cmd.h"
+
+/*
+ * END OF FILE <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+ */
+
---
+++ protocol/handling_PWM_cmd.h
@@ -0,0 +1,15 @@
+/******************************************************************************************
+ * File name	: handling_PWM_cmd.h
+ * Description	: Parsing the com port commands about PWM, and handling the PWM commands.
+ * Creator	: XU ZAN
+ * Creation date: Tue.	June 25, 2013
+ * Copyright(C)	2010 --- 2013	Hella Shanghai Electronics Co., Ltd.
+ * All rights reserved.
+ *
+ ******************************************************************************************/
+
+#ifndef HANDLING_PWM_CMD_H
+#define HANDLING_PWM_CMD_H
+
+#endif	/*  HANDLING_PWM_CMD_H  */
+
