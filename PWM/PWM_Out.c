4f071eef3c5b94144e4136d3a8f837e84169f63a
XU ZAN <Zan.Xu@hella.com>
2013-07-18 11:06:22 +0800

    在PWM_Out.c 和 timer_user.c 中实现如下函数 ：
    	Set_Config_PWM_Out_Param
    	Set_PwmOutPrimaryChn_Level
    	Set_PwmOutSecondaryChn_Level
    	PWM_Out_Start
    	PWM_Out_Stop
    
    	__interrupt void MD_INTTAA0CC0
    	__interrupt void MD_INTTAA1CC0
    
    基本实现利用2个定时器来实现PWM Out的功能， 接下来是分别设置TAA0, TAA1定时器的定时间隔，即定时开始数。 通过函数Calculate_Timer_Interval来实现。

--- PWM/PWM_Out.c
+++ PWM/PWM_Out.c
@@ -8,7 +8,7 @@
  * 
  ********************************************************************************************/
 
-#include "PWM.h"
+#include "PWM_Out.h"
 
 /*===========================================================================================
  * Introduce the 2 timer : TAA0 & TAA1.
@@ -25,10 +25,12 @@
  *                                                                                                       TAA1 timeout (TAA1 stop)
  */
 #include "../timer.h"
+#include "../port.h"
+#include "../Switch_Relay_Control/Switch_Relay_Control.h"
 
-int iCurrentActivePwmOutCh = 0;
+struct PwmOutChnSelector g_stPwmOutChn = {PWM_ATTR_PRIMARY, 0, 0x00};
 
-void Set_Config_PWM_Out_Param(ST_PWM_OUT stPwmParam)
+void Set_Config_PWM_Out_Param(ST_PWM_PARAM stPwmParam)
 {
 	/*
 	 * Firstly do initialization for the 2 timer.
@@ -42,18 +44,24 @@ void Set_Config_PWM_Out_Param(ST_PWM_OUT stPwmParam)
 	 */
 }
 
-
-void PWM_Out_Start(void)
-{
+void PWM_Out_Start(int ePrimaryOrSecondary, int ePwmChn, BYTE bytPwmBoardID)
+{	
 	TAA0_Start();
-	if (iCurrentActivePwmOutCh == 1)
+
+	if (ePrimaryOrSecondary == PWM_ATTR_PRIMARY)
 	{
-		PWM_OUT1 = HIGH;
+		Set_PwmOutPrimaryChn_Level(ePwmChn, HIGH);
 	}
-	else if (iCurrentActivePwmOutCh == 2)
+	else if (ePrimaryOrSecondary == PWM_ATTR_SECONDARY)
 	{
-		PWM_OUT2 = HIGH;
-	}	
+		Set_PwmOutSecondaryChn_Level(bytPwmBoardID, ePwmChn, HIGH);
+	}
+	
+	g_stPwmOutChn.ePrimaryOrSecondary = ePrimaryOrSecondary;
+	g_stPwmOutChn.iPwmOutChn = ePwmChn;
+	g_stPwmOutChn.bytPwmOutBoardID = bytPwmBoardID;
+	
+	return;
 }
 
 
@@ -62,23 +70,76 @@ void PWM_Out_Stop(void)
 	TAA0_Stop();
 	TAA1_Stop();
 	
-	iCurrentActivePwmOutCh = 0;	// Reset
-	
 	/*
 	 * Reset 2 PWM out channels to low level mandantorily,
 	 * whatever condition the external environment is.
 	 */
-	PWM_OUT1 = LOW;
-	PWM_OUT2 = LOW;
+	if (g_stPwmOutChn.ePrimaryOrSecondary == PWM_ATTR_PRIMARY)
+	{
+		Set_PwmOutPrimaryChn_Level(g_stPwmOutChn.iPwmOutChn, LOW);
+	}
+	else if (g_stPwmOutChn.ePrimaryOrSecondary == PWM_ATTR_SECONDARY)
+	{
+		Set_PwmOutSecondaryChn_Level(g_stPwmOutChn.bytPwmOutBoardID, g_stPwmOutChn.iPwmOutChn, LOW);
+	}
+	return;
 }
 
+/**************************************************************************************************************************/
 void Calculate_Timer_Interval(float 	fARGIN_PWM_Freq_InHz, 
 			      float 	fARGIN_PWM_DutyCycle_InPercentage,
 			      USHORT 	*ushrtARGOUT_pInterval_For_PwmON,
 			      USHORT 	*ushrtARGOUT_pInterval_For_PwmOFF)
 {
+	float fTcycle = 0.00f,
+	      fPwmOnTime = 0.00f,
+	      fPwmOffTime = 0.00f;
+	
+	fTcycle= fARGIN_PWM_Freq_InHz;
+	if ((fARGIN_PWM_DutyCycle_InPercentage>=0.00) && (fARGIN_PWM_DutyCycle_InPercentage<=1.00))
+	{
+		fPwmOnTime = fTcycle * fARGIN_PWM_DutyCycle_InPercentage;
+		fPwmOffTime = fTcycle * (1 - fARGIN_PWM_DutyCycle_InPercentage);
+	}
+	else
+	{
+		// Error
+	}
+	
+	
 }
 
+void Set_PwmOutPrimaryChn_Level(PWM_OUT_PRIMARY_CHN ePrimaryChn, enum LEVEL eLevel)
+{
+	switch (ePrimaryChn)
+	{
+	case PwmOutChn_Primary0 :
+		// CS4 = eLevel;
+		PORT_ChangeP95Output(eLevel);
+		break;
+	case PwmOutChn_Primary1 :
+		// CS5 = eLevel;
+		PORT_ChangeP94Output(eLevel);
+		break;
+	default :
+		break;
+	}
+	return;
+}
+
+void Set_PwmOutSecondaryChn_Level(BYTE 				bytPwmBoardID, 
+				  PWM_OUT_SECONDARY_CHN 	ePwmSecondaryChn, 
+				  enum LEVEL 			eLevel)
+{
+	PST_Access_Ctrl_SwitchRelayMatrix pPwmOut;
+	
+	pPwmOut->byteBoardID = bytPwmBoardID;
+	pPwmOut->dwSwitch_Relay_CHn = (DWORD)ePwmSecondaryChn;
+	pPwmOut->eOpen_Close_State = eLevel;
+	
+	Control_Single_Switch(pPwmOut);
+	return;
+}
 /*
  * END OF FILE <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
  */
---
+++ PWM/PWM_Out.h
@@ -0,0 +1,108 @@
+/********************************************************************************************
+ * File name	: PWM_Out.h
+ * Description	: PWM Out module, declares the prototypes in this file.
+ * Creator	: XU ZAN	[HSE-OT]
+ * Creation date: Fri.	June 07, 2013
+ * Copyright(C)	2010 --- 2013	Hella Shanghai Electronics Co., Ltd.
+ * All rights reserved.
+ * 
+ ********************************************************************************************/
+
+#ifndef PWM_OUT_H
+#define PWM_OUT_H
+
+
+	#include "../macrodriver.h"
+	#include "../bus.h"
+	
+	typedef struct stPWM_Param
+	{
+		float fPWM_Freq_InHz;
+		float fPWM_DutyCycle_InPercentage;
+		float fPWM_Amplitude_InVolt;	// This parameter could be reserved for extension in future.	
+	}
+	ST_PWM_PARAM, *PST_PWM_PARAM;
+	
+	/*
+	 * Note : 
+	 * Since the original PWM ports of Renesas uPD70F3376 chip had been occupied as the GPIOs in the schematics, 
+	 * in order to output PWM signal or acquire the external PWM signal, it was constrained to simulate to implement
+	 * the PWM function through software. 
+	 *
+	 * Providing 2 kinds of PWM-outputting approaches :
+	 *	1. Choose CS2(i.e. port P97), CS3(port P96), CS4(i.e. P95), CS5(i.e. P94) ports to output PWM signals
+	 *	   but here only select 2 ports : CS4 and CS5, other ports are not enabled.
+	 *
+	 *	2. Choose one pin of DataBus [D0..D23] to output PWM signal.
+	 */
+	enum PwmOutAttribute
+	{
+		PWM_ATTR_PRIMARY = 0,
+		PWM_ATTR_SECONDARY
+	};
+	// PWM Approach 1 :
+	typedef enum PwmOutPrimaryChn
+	{
+		PwmOutChn_Primary0 = 0,	// CS4
+		PwmOutChn_Primary1 = 1	// CS5
+	}
+	PWM_OUT_PRIMARY_CHN;
+	// PWM Approach 2 :
+	typedef enum PwmOutSecondaryChn
+	{
+		PwmOutChn_Secondary0,	// DB00
+		PwmOutChn_Secondary1,	// DB01
+		PwmOutChn_Secondary2,	// DB02
+		PwmOutChn_Secondary3,	// DB03
+		PwmOutChn_Secondary4,	// DB04
+		PwmOutChn_Secondary5,	// DB05
+		PwmOutChn_Secondary6,	// DB06
+		PwmOutChn_Secondary7,	// DB07
+		PwmOutChn_Secondary8,	// DB08
+		PwmOutChn_Secondary9,	// DB09
+		PwmOutChn_Secondary10,	// DB10
+		PwmOutChn_Secondary11,	// DB11
+		PwmOutChn_Secondary12,	// DB12
+		PwmOutChn_Secondary13,	// DB13
+		PwmOutChn_Secondary14,	// DB14
+		PwmOutChn_Secondary15,	// DB15
+		PwmOutChn_Secondary16,	// DB16
+		PwmOutChn_Secondary17,	// DB17
+		PwmOutChn_Secondary18,	// DB18
+		PwmOutChn_Secondary19,	// DB19
+		PwmOutChn_Secondary20,	// DB20
+		PwmOutChn_Secondary21,	// DB21
+		PwmOutChn_Secondary22,	// DB22
+		PwmOutChn_Secondary23	// DB23
+	}
+	PWM_OUT_SECONDARY_CHN;
+	/************************************************************************************************/
+	struct PwmOutChnSelector
+	{
+		enum PwmOutAttribute 	ePrimaryOrSecondary;
+		int  			iPwmOutChn;
+		BYTE 			bytPwmOutBoardID;
+	};
+	
+	/*===============================================================================================
+	 * Functions prototype relative to PWM :
+	 */
+	void Set_Config_PWM_Out_Param(ST_PWM_PARAM stPwmParam);
+	
+	void Set_PwmOutPrimaryChn_Level(PWM_OUT_PRIMARY_CHN ePrimaryChn, enum LEVEL eLevel);
+	
+	void Set_PwmOutSecondaryChn_Level(BYTE 				bytPwmBoardID, 
+					  PWM_OUT_SECONDARY_CHN 	ePwmSecondaryChn, 
+					  enum LEVEL 			eLevel);
+	
+	void PWM_Out_Start(int ePrimaryOrSecondary, int ePwmChn, BYTE bytPwmBoardID);
+	void PWM_Out_Stop(void);
+	
+	void Calculate_Timer_Interval(float 	fARGIN_PWM_Freq_InHz, 
+				      float 	fARGIN_PWM_DutyCycle_InPercentage,
+				      USHORT 	*ushrtARGOUT_pInterval_For_PwmON,
+				      USHORT 	*ushrtARGOUT_pInterval_For_PwmOFF);
+	
+
+#endif	/*  PWM_OUT_H  */
+
