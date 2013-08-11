20083efff9972eb4369f26865ba3d60899ae999e
hseres11 <Zan.Xu@hella.com>
2013-08-11 16:28:26 +0800

    Aux_Testing項目是用來輔助測試/調試FW_Simulation_Testing.vcxproj項目的，用來編寫一些輔助測試用例和輔助設計驗證一些算法的。

---
+++ FW_Simulation_Testing/Aux_Testing/Aux_Testing.h
@@ -0,0 +1,16 @@
+/***********************************************************************************************************
+ * File name	: Aux_Testing.h
+ * Description	: Design the auxiliary test instances in the Aux_Testing.c (.h) program pair. 
+ * Creator		: XU ZAN	[HSE-OT]
+ * Creation date: Fri.	Aug. 02, 2013
+ * Copyright(C)		2013	Hella Shanghai Electronics Co., Ltd.
+ * All rights reserved.
+ *
+ ***********************************************************************************************************/
+
+#ifndef AUX_TESTING_H
+#define AUX_TESTING_H
+
+	void Aux_Testing_Instances(void);
+
+#endif	/*  AUX_TESTING_H  */
