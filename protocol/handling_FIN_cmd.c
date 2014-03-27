248dad8512765a4a511d68e59ce6e59ae988a5ae
XU ZAN <zan.xu@hella.com>
2014-03-27 20:57:32 +0800

    Add 2 program files to implement the "handling_FIN_cmd" new protocol commands.

---
+++ protocol/handling_FIN_cmd.c
@@ -0,0 +1,12 @@
+/*************************************************************************************************************************
+ * File name		: handling_FIN_cmd.h
+ * Description		: To handle the "FIN" type of commands, in order to support the Optical-couple-INput board.
+ * Creator			: XU ZAN	[HSE-OT]
+ * Creation date	: Thur.		March 27, 2014
+ * Copyright(C)		2014	Hella Shanghai Electronics Co., Ltd.
+ * All rights reserved.
+ *
+ *************************************************************************************************************************/
+
+
+#include "handling_FIN_cmd.h"
---
+++ protocol/handling_FIN_cmd.h
@@ -0,0 +1,15 @@
+/*************************************************************************************************************************
+ * File name		: handling_FIN_cmd.h
+ * Description		: To handle the "FIN" type of commands, in order to support the Optical-couple-INput board.
+ * Creator			: XU ZAN	[HSE-OT]
+ * Creation date	: Thur.		March 27, 2014
+ * Copyright(C)		2014	Hella Shanghai Electronics Co., Ltd.
+ * All rights reserved.
+ *
+ *************************************************************************************************************************/
+
+
+#ifndef HANDLING_FIN_CMD_H
+#define HANDLING_FIN_CMD_H
+
+#endif	/*  HANDLING_FIN_CMD_H  */
